8bb99770b931db6ea0b12a82bfed66cc58efafd3
ereminmaksim <eremin1988ma21@gmail.com>
2022-04-15 15:59:48 +0300

    Готовые Link`/ страница входа / thirdweb закинуты в dashboard v.2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import {Collection} from "../typing";
-import ApesComponent from "./apes";
+// import ApesComponent from "./apes";
 import {GetServerSideProps} from "next";
-import {sanityClient} from "../sanity";
-
+import {sanityClient, urlFor} from "../sanity";
+import Link from "next/link";
 
 
 export interface CollectionType {
@@ -27,9 +27,47 @@ const Home = ({collections, collectionsTwo}: CollectionType) => {
                 <span className='font-extrabold  text-gray-700
                             '> Сapuchin</span>
             </h1>
-            <ApesComponent
-                collections={collections}
-                collectionsTwo={collectionsTwo}/>
+            {/*<ApesComponent*/}
+            {/*    collections={collections}*/}
+            {/*    collectionsTwo={collectionsTwo}/>*/}
+            <main className="bg-state-100 p-10 shadow-2xl shadow-gray-400/50">
+                <div className='grid mx-20 md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-2'>
+                    <div>
+                        {collections.map(collection => (
+                            <div key={collection._id}>
+                                <Link href={`nft/${collection.slug.current}`}>
+                                    <div className='flex flex-col items-center transition-all duration-200
+                        hover:scale-105 cursor-pointer'>
+
+                                        <img className='h-96 w-60 rounded-2xl object-cover'
+                                             src={urlFor(collection.mainImage).url()} alt="image"/>
+                                        <div className='p-5'>
+                                            <h2 className='text-3xl'>{collection.title}</h2>
+                                            <p className='mt-2 text-sm text-gray-400'>{collection.description}</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
+                    {collectionsTwo.map(collection => (
+                        <div key={collection._id}>
+                            <Link href={`nft/${collection.slug.current}`}>
+                                <div className='flex flex-col items-center transition-all duration-200
+                        hover:scale-105 cursor-pointer'>
+
+                                    <img className='h-96 w-60 rounded-2xl object-cover'
+                                         src={urlFor(collection.mainImage).url()} alt="image"/>
+                                    <div className='p-5'>
+                                        <h2 className='text-3xl'>{collection.title}</h2>
+                                        <p className='mt-2 text-sm text-gray-400'>{collection.description}</p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </main>
         </div>
     )
 }
@@ -96,4 +134,4 @@ export const getServerSideProps: GetServerSideProps = async () => {
         }
     }
 };
-
+// https://ereminnftcryptoapp.sanity.studio/
